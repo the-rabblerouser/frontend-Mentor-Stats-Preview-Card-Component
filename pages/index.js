9a7b89fc2a906690b8cf23511d85ff455e17b5c8
Joe McCann <joemccann@Joes-MBP.home>
2021-11-18 17:06:31 -0500

    container div created for content

--- pages/index.js
+++ pages/index.js
@@ -1,9 +1,21 @@
+import styled from 'styled-components';
+
 export default function Home() {
+	const Container = styled.div`
+		display: flex;
+		justify-content: center;
+		align-items: center;
+		height: 100vh;
+		width: 100vw;
+	`;
+
 	return (
 		<>
-			<h1 style={{ color: 'hsl(0, 0%, 100%)', fontFamily: 'Inter' }}>
-				hello world
-			</h1>
+			<Container>
+				<h1 style={{ color: 'hsl(0, 0%, 100%)', fontFamily: 'Inter' }}>
+					hello world
+				</h1>
+			</Container>
 		</>
 	);
 }
