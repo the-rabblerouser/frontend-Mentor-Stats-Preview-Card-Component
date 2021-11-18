0b8bba74665989cffac229d5d3c84810c4934218
Joe McCann <joemccann@Joes-MBP.home>
2021-11-18 17:10:53 -0500

    card component created and styled

--- pages/index.js
+++ pages/index.js
@@ -9,12 +9,21 @@ export default function Home() {
 		width: 100vw;
 	`;
 
+	const Card = styled.div`
+		background: hsl(244, 38%, 16%);
+		border-radius: 10px;
+		height: 42rem;
+		width: 21rem;
+	`;
+
 	return (
 		<>
 			<Container>
-				<h1 style={{ color: 'hsl(0, 0%, 100%)', fontFamily: 'Inter' }}>
-					hello world
-				</h1>
+				<Card>
+					<h1 style={{ color: 'hsl(0, 0%, 100%)', fontFamily: 'Inter' }}>
+						hello world
+					</h1>
+				</Card>
 			</Container>
 		</>
 	);
