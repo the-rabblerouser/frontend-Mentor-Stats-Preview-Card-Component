7eadb8542637404c763a3b6ce44d77d2f94ffc86
Joe McCann <joemccann@Joes-MBP.home>
2021-11-18 18:10:01 -0500

    image and image overlay styled-components added to card

--- pages/index.js
+++ pages/index.js
@@ -1,3 +1,7 @@
+import Image from 'next/image';
+
+import cardHeader from '../public/image-header-mobile.jpg';
+
 import styled from 'styled-components';
 
 export default function Home() {
@@ -11,18 +15,31 @@ export default function Home() {
 
 	const Card = styled.div`
 		background: hsl(244, 38%, 16%);
-		border-radius: 10px;
+		bordert-radius: 10px;
 		height: 42rem;
 		width: 21rem;
+	`;
+
+	const ImageOverlay = styled.div`
+		background: hsl(277, 64%, 61%);
+		position: absolute;
+		height: 15.45rem;
+		width: 21rem;
+		z-index: 1;
+		border-radius: 10px 10px 0 0;
+		filter: opacity(0.55) brightness(0.4) saturate(3.1);
+	`;
+
+	const StyledImage = styled(Image)`
+		border-radius: 10px 10px 0 0;
 	`;
 
 	return (
 		<>
 			<Container>
 				<Card>
-					<h1 style={{ color: 'hsl(0, 0%, 100%)', fontFamily: 'Inter' }}>
-						hello world
-					</h1>
+					<ImageOverlay />
+					<StyledImage src={cardHeader} height={480} />
 				</Card>
 			</Container>
 		</>
