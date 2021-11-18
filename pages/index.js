7f00ca80ab9745e3e554bded45ca30d4913d6cd0
Joe McCann <joemccann@Joes-MBP.home>
2021-11-18 18:40:25 -0500

    next.js boiler code to solve ssr issue between next.js and styled-components

--- pages/index.js
+++ pages/index.js
@@ -16,14 +16,14 @@ export default function Home() {
 	const Card = styled.div`
 		background: hsl(244, 38%, 16%);
 		bordert-radius: 10px;
-		height: 42rem;
+		height: 50rem;
 		width: 21rem;
 	`;
 
 	const ImageOverlay = styled.div`
 		background: hsl(277, 64%, 61%);
 		position: absolute;
-		height: 15.45rem;
+		height: 15.42rem;
 		width: 21rem;
 		z-index: 1;
 		border-radius: 10px 10px 0 0;
@@ -34,12 +34,53 @@ export default function Home() {
 		border-radius: 10px 10px 0 0;
 	`;
 
+	const TextContainer = styled.div`
+		display: flex;
+		flex-direction: column;
+		height: 100%;
+		width: 100%;
+		padding: 1rem 1.5rem 1rem 1.5rem;
+	`;
+
+	const MainHeading = styled.h1`
+		font-family: inter;
+		font-weight: 700;
+		font-size: 1.8rem;
+		text-align: center;
+		color: hsl(0, 0%, 100%);
+	`;
+
+	const Span = styled.span`
+		font-family: inter;
+		font-weight: 700;
+		color: hsl(277, 64%, 61%);
+	`;
+
+	const MainParagraph = styled.p`
+		font-family: Lexend Deca;
+		font-weight: 300;
+		text-align: center;
+		line-height: 1.8rem;
+		font-size: 15px;
+		color: hsla(0, 0%, 100%, 0.75);
+		margin: 0 0.8rem 0 0.8rem;
+	`;
+
 	return (
 		<>
 			<Container>
 				<Card>
 					<ImageOverlay />
 					<StyledImage src={cardHeader} height={480} />
+					<TextContainer>
+						<MainHeading>
+							Get <Span>insights</Span> that help your business grow.
+						</MainHeading>
+						<MainParagraph>
+							Discover the benefits of data analytics and make better decisions
+							regarding revenue, customer experience, and overall efficiency.
+						</MainParagraph>
+					</TextContainer>
 				</Card>
 			</Container>
 		</>
